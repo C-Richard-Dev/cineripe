289f04ca0bd13a77512ed8dfe9cc01e34276321f
Caio Richard <richarddeabreu2017@gmail.com>
2025-10-13 13:51:53 -0300

    fix: correção de erro

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -31,6 +31,22 @@ class FavoriteController extends Controller
         return view('pages.favorites.index', compact('movies'));
     }
 
+    public function store (Request $request, int $movie){
+        $movieAlreadyAdd = $request->user()->favorites()->where('tmdb_id', $movie)->first();
+        if ($movieAlreadyAdd){ // verificando se o filme já
+            return redirect()->back()->with('success', 'Opa! Esse filme já está na sua lista de favoritos!');
+        }
+        try{
+            Favorite::create([
+                'user_id' => Auth::id(),
+                'tmdb_id' => $movie,
+            ]);
+            return redirect()->back()->with('success', 'O filme foi adicionado á sua lista de favoritos!');
+        } catch(\Exception $e){
+            return redirect()->back()->with('error', 'Ops... Ocorreu um erro ao adicioná-lo á sua lista de favoritos: '.$e->getMessage());
+        }
+    }
+
 
     public function destroy(Request $request, int $movie){
         $favorite = $request->user()->favorites()->where('tmdb_id', $movie)->first();
